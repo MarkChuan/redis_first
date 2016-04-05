25a8208c73bee7235f893c8abaf6f4c397482066
mark <383282199@qq.com>
2016-04-05 11:46:24 +0800

    redis基本环境搭建，功能测试。缓存框架

---
+++ src/com/test/MyTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyTest {
+
+	
+	
+	//mybatis  高级映射一对多查询
+    @RequestMapping("/index.html")    
+   public void selectUser(){    
+   	
+   	 
+   	 System.out.println("----------------（一对多测试）-----------------状态----");	
+	  System.out.println("----------------（一对多测试）-----------------状态----");	
+			    	       
+   }
+}
